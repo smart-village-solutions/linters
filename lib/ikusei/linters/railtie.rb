dd75379dcf679cf6fd2e459d3ec408220051e26e
Holger Frohloff <h.frohloff@ikusei.de>
2016-10-17 16:36:59 +0200

    Try another way of loading Railtie

--- lib/ikusei/linters/railtie.rb
+++ lib/ikusei/linters/railtie.rb
@@ -1,6 +1,10 @@
-require "rails"
-class Ikusei::Linters::Railtie < Rails::Railtie
-  rake_tasks do
-    load "lib/ikusei/tasks/lint.rake"
+module Ikusei
+  if defined? Rails::Railtie
+    require "rails"
+    class Railtie < Rails::Railtie
+      rake_tasks do
+        load "ikusei/tasks/lint.rake"
+      end
+    end
   end
 end
